109a1c9b1fb602906296abb712a472503db08ebc
danmcCs111 <danmccs111@gmail.com>
2026-05-17 11:38:04 -0400

    add centering on screen

--- src/main/java/JoystickConsole.java
+++ src/main/java/JoystickConsole.java
@@ -1,6 +1,9 @@
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Point;
+import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -48,12 +51,31 @@ public class JoystickConsole extends JFrame
 		
 //		this.add(scrollPane, BorderLayout.CENTER);
 		this.add(closeButton);
+		centerOnScreen(this);
 		this.setVisible(true);
 	}
 	
 	public void addOutput(String out)
 	{
 //		consoleOutput.setText(out + "\n");//just latest output to save memory.
+	}
+	
+	public static void centerOnScreen(Component comp)
+	{
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		Point loc = new Point(0, 0);
+		
+		double 
+			rw = screenSize.getWidth(),
+			rh = screenSize.getHeight(),
+			w = comp.getWidth(),
+			h = comp.getHeight();
+		int
+			x = loc.x + (int)((rw/2.0) - (w / 2.0)),
+			y = loc.y + (int)((rh/2.0) - (h/2.0));
+		y=(y<0)?0:y;
+		
+		comp.setLocation(x, y);
 	}
 
 }
