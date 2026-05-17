38c82683acedf484e54c624724ad9508395517b1
danmcCs111 <danmccs111@gmail.com>
2026-05-17 10:52:12 -0400

    remove output and replace with close button to reduce memory consumption

--- src/main/java/JoystickConsole.java
+++ src/main/java/JoystickConsole.java
@@ -1,22 +1,27 @@
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 
 public class JoystickConsole extends JFrame 
 {
 	private static final long serialVersionUID = 1L;
 	
 	private static final String
-		TITLE = "Joystick Output Console";
+		TITLE = "Joystick Output Console",
+		CLOSE_BTN_TEXT = "Close";
 	private static final Dimension
-		MIN_DIMENSION = new Dimension(500, 400);
-	private JScrollPane
-		scrollPane = new JScrollPane();
-	private JTextArea
-		consoleOutput = new JTextArea();
+		MIN_DIMENSION = new Dimension(350, 100);
+//	private JScrollPane
+//		scrollPane = new JScrollPane();
+//	private JTextArea
+//		consoleOutput = new JTextArea();
+	private JButton
+		closeButton;
 	
 	public JoystickConsole()
 	{
@@ -25,20 +30,30 @@ public class JoystickConsole extends JFrame
 	
 	private void buildWidgets()
 	{
-		scrollPane.setViewportView(consoleOutput);
+//		scrollPane.setViewportView(consoleOutput);
 		
+		closeButton = new JButton(CLOSE_BTN_TEXT);
+		closeButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
 		this.setTitle(TITLE);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setLayout(new BorderLayout());
+		FlowLayout fl = new FlowLayout();
+		fl.setAlignment(FlowLayout.CENTER);
+		this.setLayout(fl);
 		this.setMinimumSize(MIN_DIMENSION);
-		this.add(scrollPane, BorderLayout.CENTER);
+		
+//		this.add(scrollPane, BorderLayout.CENTER);
+		this.add(closeButton);
 		this.setVisible(true);
 	}
 	
 	public void addOutput(String out)
 	{
-		consoleOutput.append(out + "\n");
-		consoleOutput.setCaretPosition(consoleOutput.getText().length()-1);
+//		consoleOutput.setText(out + "\n");//just latest output to save memory.
 	}
 
 }
